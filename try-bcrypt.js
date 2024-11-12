9d72de91475eaea76c5f7828939c535442cec3c4
Khem <tempkhem@gmail.com>
2024-11-12 14:29:24 +0700

    feat(example): salt for bcrypt

--- try-bcrypt.js
+++ try-bcrypt.js
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt";
+
+const salt = "secret-character-you-like";
+
 const saltRounds = 10;
 const myPlaintextPassword = "JSD8*password";
 
@@ -6,23 +9,23 @@ const saltRounds2 = 2;
 const myPlaintextPassword2 = "myPa$$wor3";
 
 /* Async encryption */
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+bcrypt.hash(myPlaintextPassword + salt, saltRounds, function(err, hash) {
   console.log("hash 1 = ", hash);
 });
-bcrypt.hash(myPlaintextPassword2, saltRounds2, function(err, hash) {
+bcrypt.hash(myPlaintextPassword2 + salt, saltRounds2, function(err, hash) {
   console.log("hash 2 = ", hash);
 });
 
 /* Sync encryption */
-const hashSync = bcrypt.hashSync(myPlaintextPassword, 10);
+const hashSync = bcrypt.hashSync(myPlaintextPassword + salt, 10);
 console.log(`hashSync 1 = ${hashSync}`);
 
-const hashSync2 = bcrypt.hashSync(myPlaintextPassword2, 2);
+const hashSync2 = bcrypt.hashSync(myPlaintextPassword2 + salt, 2);
 console.log(`hashSync 2 = ${hashSync2}`);
 
 /* Sync integrity */
-const isMatchPassword = bcrypt.compareSync(myPlaintextPassword, hashSync); // true
-const isMatchPassword2 = bcrypt.compareSync("eieiPassword", hashSync);     // false
+const isMatchPassword = bcrypt.compareSync(myPlaintextPassword + salt, hashSync); // true
+const isMatchPassword2 = bcrypt.compareSync(myPlaintextPassword2, hashSync);     // false
 
 console.log('isMatchPassword = ', isMatchPassword);
 console.log('isMatchPassword2 = ', isMatchPassword2);
